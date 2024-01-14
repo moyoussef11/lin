06845336ddfc96b969b0d978b0be3d2a7c4b0e15
moyoussef11 <mohmedyoussef472@gmail.com>
2024-01-14 12:42:39 +0200

    change color

--- src/components/soon/Soon.jsx
+++ src/components/soon/Soon.jsx
@@ -9,7 +9,7 @@ const Soon = () => {
   return (
     <>
       <div className='w-100 vh-100 d-flex align-items-center justify-content-center flex-column bg-dark-subtle'>
-              <div><h6 className='text-uppercase'>{t("soon")}</h6></div>
+              <div><h6 className='text-secondary'>{t("soon")}</h6></div>
               <div>
                   <span><TbTruckLoading size={30} color='red' className='d-block mx-auto' /></span>
                   <Link className='nav-link text-capitalize bg-info p-3 rounded-3 text-white mt-2' to='/'>
